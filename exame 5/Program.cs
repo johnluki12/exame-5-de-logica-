88acf6b9facacfcad52c2654a53315f3acf2835a
joaolucastbr@gmail.com <joaolucastbr@gmail.com>
2022-07-09 17:02:02 -0300

    Adicionar arquivos de projeto.

---
+++ exame 5/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace exame_5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int i;
+            Console.WriteLine(" Números pares de 0 á 100 :");
+            for (i = 1; i < 100; i++)
+            {
+                if (i % 2 == 0)
+                    Console.WriteLine(i);
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
